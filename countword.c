6afd1573cb4a50d5987e20f4413a070738542fe8
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-03-02 02:21:15 -0800

    c pgm
    good

---
+++ countword.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int count,i=0;
+char string[100];
+clrscr();
+printf("\n Enter the sentence:");
+scanf("[^\n]"s);
+for(i=0;i<=100;i++)
+{
+if(si=..)
+{
+ count=++;
+ }
+ printf("number of words in the sentence:%d")
+ }
+ }
+
+ 
